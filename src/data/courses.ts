f9a1b20f326ff2f46b268ec3ef5e6854075d0e4c
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-06 03:40:15 +0600

    Use tech stack vite_react_shadcn_ts

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -8,7 +8,7 @@ export interface CourseOption {
 export const coursesData: CourseOption[] = [
   { name: "Competitive Programming", code: "CS200", creditHours: 3 },
   { name: "Algorithm Design and Analysis", code: "CS231", creditHours: 3 },
-  { name: "Algorithms", code: "CS301", creditHours: 4 },
+  { name: " Algorithm Design and Analysis Lab", code: "CS232", creditHours: 1.5 },
   { name: "Database Systems", code: "CS305", creditHours: 3 },
   { name: "Web Development", code: "CS325", creditHours: 3 },
   { name: "Mobile App Development", code: "CS330", creditHours: 3 },
